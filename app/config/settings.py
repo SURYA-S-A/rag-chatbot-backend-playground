af3a9fb0bd96aeef0e15e221a40e9350daa90ea0
SURYA <15arul.surya16@gmail.com>
2025-09-25 11:31:09 +0530

    Refactor code structure for improved readability and maintainability

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    LLM_API_KEY: str
+    LLM_API_KEY: Optional[str] = None
     LLM_PROVIDER: str
     LLM_MODEL: str
     QDRANT_API_KEY: str
